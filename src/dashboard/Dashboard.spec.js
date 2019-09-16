6628cd073bbd8ea1dff1c1a6241fdd0ffdd6df83
ampersand <ampersand@ampersands-MacBook-Pro.local>
2019-09-16 16:10:15 -0700

    test 1?

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,13 +1,20 @@
 // Test away
 import React from 'react';
-import renderer from 'react-test-renderer';
+import { render } from '@testing-library/react';
+// import renderer from 'react-test-renderer';
 
 import Dashboard from './Dashboard';
 
+{ /*
 describe ('<Dashboard />', () => {
     it('should match snapshot', () => {
         const tree = renderer.create(<Dashboard />).toJSON();
 
         expect(tree).toMatchSnapshot();
     })
+})
+*/}
+
+test('Render Dashboard', () => {
+    render (<Dashboard />);
 })
