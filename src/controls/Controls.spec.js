0ce21e140e6f0ac89c0a1c3438524efd85dde059
ampersand <ampersand@ampersands-MacBook-Pro.local>
2019-09-17 11:40:53 -0700

    control

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,42 @@
 // Test away!
+
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+
+import Controls from './Controls';
+
+test('Render Controls', () => {
+
+    render(<Controls />);
+});
+
+
+test('should match snapshot', () => {
+    expect(render(<Controls />)).toMatchSnapshot();
+});
+
+
+test('Control Buttons', () => {
+    const locked = jest.fn();
+    const closed = jest.fn();
+    const toggleLocked = jest.fn();
+    const toggleClosed = jest.fn();
+    
+    const { getByText } = render(
+            <Controls
+                locked={locked}
+                closed={closed}
+                toggleLocked={toggleLocked}
+                toggleClosed={toggleClosed}
+            />);
+    
+    const lockButton = getByText(/Unlock Gate/i);
+    const gateButton = getByText(/Open Gate/i);
+    
+    fireEvent.click(lockButton);
+    fireEvent.click(gateButton);
+    expect(toggleLocked).toHaveBeenCalled();
+    // expect(toggleClosed).toHaveBeenCalled(); <-- doesn't work?
+    
+    });
+    
